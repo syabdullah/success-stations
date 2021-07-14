74569e78a9252510e936043120afc0f851798351
Codility AppleID <codilityappleid@vig38-h03-89-89-89-44.dsl.sta.abo.bbox.fr>
2021-07-14 10:58:13 +0500

    remove code

--- src/redux/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import updateProfile from '../ReduxReducer';
-const rootReducer = combineReducers(
-{ updateProfile: updateProfile }
-);
-const configureStore = () => {
-return createStore(rootReducer);
-}
-export default configureStore;
--- src/util/Helper.js
+++
@@ -1,7 +0,0 @@
-export default class Helper {
-
-    static isEmailValid(email) {
-       let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-       return reg.test(email) == 0;
-  }
-}
