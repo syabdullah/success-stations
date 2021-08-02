7151720b3609c26c91a360cde23a665bc262d15c
Junaid Tahir <muhammad.junaid@codilitysolutions.com>
2021-08-02 13:00:41 +0500

    inbox created

--- lib/view/messages/inbox.dart
+++ lib/view/messages/inbox.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:success_stations/styling/colors.dart';
+import 'package:success_stations/styling/images.dart';
+import 'package:success_stations/styling/text_style.dart';
 
 class Inbox extends StatefulWidget {
   _InboxState createState() => _InboxState();
@@ -18,10 +20,43 @@ class _InboxState extends State<Inbox> {
             height: Get.height,
             width: Get.width,
             color: AppColors.appBarBackGroundColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top:45,left: 12),
+                  child: Row(
+                    children: [
+                        Image.asset(AppImages.arrowBack),
+                        Center(
+                          widthFactor:5,
+                          child: Text("INBOX",
+                          style:AppTextStyles.appTextStyle(fontSize: 18, fontWeight: FontWeight.bold, color:Colors.white,),
+                        ),
+                        ),
+                    ],
+                  ),
+                ),
+                 Padding(
+                   padding: const EdgeInsets.only(top:30.0,left:20),
+                   child: Text("Recently Contacted",
+                     style:AppTextStyles.appTextStyle(fontSize: 18, fontWeight: FontWeight.normal, color:Colors.white,),
+                    ),
+                 ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(top:5.0,),
+                    // color: Colors.white,
+                    height: Get.height,
+                    child: recentlyContacted()
+                
+                  ),
+                )     
+              ],
+            )
           ),
-          messageList(
-
-          ),
+          messageList(),
+          
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -34,13 +69,92 @@ class _InboxState extends State<Inbox> {
     );
   }
   Widget messageList() {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(topLeft:Radius.circular(10),topRight:Radius.circular(110)),
-      child: Container(
-        margin: EdgeInsets.only(top:Get.height/3.0),
-        height: Get.height/1.3,
-        color: Colors.orange,
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(topLeft:Radius.circular(50),topRight:Radius.circular(50)),
+           color: Colors.white,
       ),
+      margin: EdgeInsets.only(top:Get.height/3.0),
+      height: Get.height/1.2,
+      child: ListView.builder(
+      itemCount: 16,
+      itemBuilder: (context, index) {
+      return ListTile(
+          title: Padding(
+            padding: const EdgeInsets.only(top:10.0),
+            child: chatListView(),
+          )
+    );
+  },
+),
     );
   }
+}
+
+Widget chatListView(){
+  return ListTile(
+    title: Row(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 30,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(60.0),
+            child: Image.asset(AppImages.profile)),
+        ),
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(left:12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("User Name",
+                  style:TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 7,),
+                 Text("Really ? that's great We will do it Tommorow",
+                  style:TextStyle(color: Colors.grey,fontSize: 13,),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+    trailing: Column(
+      children: [
+        Text("2:30pm",
+          style:TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),
+         ),
+      ],
+    ),
+    );
+}
+Widget recentlyContacted(){
+  return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 16,
+      itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.only(left:25.0,),
+        child: recentChat(),
+      );});
+}
+Widget recentChat(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 30,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(60.0),
+            child: Image.asset(AppImages.profile)),
+        ),
+        SizedBox(height: 5,),
+        Text("User Name",style: TextStyle(fontSize: 10,color: Colors.white),
+        )
+    ],
+  );
+  
 }
