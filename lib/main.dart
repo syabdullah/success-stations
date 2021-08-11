d8b0b97ea716572f17267b33e5372927cdab36e1
LAP-073 <lap-073@vig38-h03-89-89-89-30.dsl.sta.abo.bbox.fr>
2021-08-11 18:20:54 +0500

    add location added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:success_stations/utils/routes.dart';
+import 'package:success_stations/view/google_map/mapview.dart';
 import 'package:success_stations/view/i18n/app_language.dart';
 import 'view/auth/forgot/forgot_password.dart';
 var auth;
@@ -39,7 +40,7 @@ class SuccessApp extends StatelessWidget {
         fallbackLocale: LocalizationServices.fallbackLocale,
         translations: LocalizationServices(),
         theme: ThemeData(primaryColor: Color(0xFF1C1719), accentColor: Colors.black,),
-          //  home: ForgotPassword(),
+          //  home: MapView(),
         initialRoute: auth == null ?  '/langua' : '/tabs',
         onGenerateRoute: SuccessStationRoutes.successStationRoutes,
         // home: NotificationPage(),
---
+++ lib/view/google_map/my_locations.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:success_stations/styling/app_bar.dart';
+import 'package:success_stations/styling/bottom_bar.dart';
+import 'package:success_stations/styling/images.dart';
+import 'package:success_stations/view/drawer_screen.dart';
+import 'package:success_stations/view/google_map/add_locations.dart';
+
+class MyLocations extends StatefulWidget {
+  const MyLocations({ Key? key }) : super(key: key);
+
+  @override
+  _MyLocationsState createState() => _MyLocationsState();
+}
+
+class _MyLocationsState extends State<MyLocations> {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CustomBottomBar(),
+      key: _scaffoldKey,
+    appBar: AppBar(backgroundColor:Colors.blue,title: Text('MY LOCATION'),centerTitle: true,),
+      
+      body: Stack(
+        children: [
+          Container(
+            height: Get.height,
+            width: Get.width,
+            child: Column(
+              children: [
+                  Container(
+                     margin: EdgeInsets.only(left:10),
+                    height: 70,
+                    child: GestureDetector(
+                      onTap:(){
+                        Get.to(AddLocations());
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.add),
+                          SizedBox(width:10),
+                          Text("Add New Location")
+                        ],
+                      ),
+                    ),
+                  ),
+
+                Expanded(
+                  child: myAddsList()),
+              ],
+            )),
+        ],
+      ),
+    );
+  }
+  
+   Widget myAddsList() {
+    return ListView.builder(
+      padding: EdgeInsets.all(5),
+      itemCount: 10,
+      // ignore: non_constant_identifier_names
+      itemBuilder: (BuildContext,index) {
+        return Card(
+          child: Container(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Center(
+                      child: Container(
+                        child: Padding(
+                          padding:
+                          const EdgeInsets.all(10.0),
+                          child: GestureDetector(
+                            child: Image.asset(
+                              AppImages.profileBg
+                            ),
+                          ),
+                        )
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment:CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Title number 1',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight:FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(height: 7),
+                          Row(
+                            children: [
+                              // Image.asset(AppImages.location, height:15),
+                              Text("Street 6k, ichra colony shabzar \n market, Gulberg \n Lahore Pakistan",textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                            ],
+                          ),
+                          // SizedBox(height: 8),
+                          //   Row(
+                          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          //     children: [
+                          //       Image.asset(AppImages.location, height:15),
+                          //       Text(
+                          //         "Al-Hajri",
+                          //         style: TextStyle(
+                          //           color: Colors.grey[300]
+                          //         ),
+                          //       ),
+                          //     ],
+                          //   ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height:20),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right:5),
+                        child: Image.asset(AppImages.blueHeart, height: 20)
+                      ),
+                      Image.asset(AppImages.call, height: 20),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          );
+        },
+    );
+  }
+}
