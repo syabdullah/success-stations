7cce44d5b3f752686fe14cc39c3ac66a2d924acf
sheeza Tariq <sheeza.tariq@codilitysolutions.com>
2021-08-17 15:48:13 +0500

    images added in fav

--- lib/action/favorite_action.dart
+++ lib/action/favorite_action.dart
@@ -4,12 +4,8 @@ import 'package:success_stations/utils/config.dart';
 
 Future<http.Response> favorite() async {
   await ApiHeaders().getData();
-  print("Apuiiii header Token ${ApiHeaders().headersWithToken}");
   final Config config = Config();
-  print("jkdhkjdhkjdhhj....!!!!!!! $config");
   var url = Uri.parse("${config.baseUrl}favorites");
-  print("url printed of the friend list ........$url");
   http.Response response = await http.get(url, headers: ApiHeaders().headersWithToken);
-  print(",d.sdm.s,dmlkdjlkjdkljekl$response");
   return response;
 }
