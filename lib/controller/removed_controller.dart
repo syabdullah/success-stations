d7759db6a84eaf48f11d1d42ae5c9772cee9693b
sheeza Tariq <sheeza.tariq@codilitysolutions.com>
2021-08-21 19:31:08 +0500

    remove fvr8 adds

--- lib/controller/removed_controller.dart
+++ lib/controller/removed_controller.dart
@@ -4,9 +4,13 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:success_stations/action/ads_action.dart';
+import 'package:success_stations/controller/favorite_controller.dart';
+import 'package:success_stations/styling/colors.dart';
 import 'package:success_stations/utils/snack_bar.dart';
 
 class RemoveController extends GetxController{
+    // final remoControllerFinal = Get.put(RemoveController());
+   final fContr = Get.put (FavoriteController());
   bool isLoading = false; 
   var regionData;
   var removingList;
@@ -21,18 +25,21 @@ class RemoveController extends GetxController{
     print(".........remove.......profileAdsRemove....profileAdsRemove....profileAdsRemove......App.........$id");
     isLoading = true ;
     await removeAdsFav(id).then((res) {
-      print("!!!!!!!!!!!!!!!!!!!!!!>>>>>>>>>>!!!!!!!!!!!!!!!!$id .............>>>>${res.body}");
-      removingList = jsonDecode(res.body);
-      print("remove adds ..........remove adds......$removingList");
-      // if(removingList['success'] == true) {
-      //   print(".....!!!!!!!!!!!!!!!!!!!!!!if Condition");
-      //   // if(userId !=null)
-      //   // profileAds(userId);
-      //   SnackBarWidget().showToast("", removeAds['message']); 
-      // }
-      // Get.off(repetedCon);
-      //  print("/././././.-----$userAds");
-      isLoading = false;
+      if(res.statusCode == 200 ||res.statusCode < 400 ){
+        print("!!!!!!!!!!!!!!!!!!!!!!>>>>>>>>>>!!!!!!!!!!!!!!!!$id .............>>>>${res.body}");
+        removingList = jsonDecode(res.body);
+        print("remove adds ..........remove adds......$removingList");
+        if(removingList['success']== true){
+          Get.snackbar(removingList['message'],'',backgroundColor: AppColors.appBarBackGroundColor);
+          fContr.favoriteList();
+          isLoading = false;
+
+        }
+
+      }
+      
+      
+      
     });
     update();
   } 
