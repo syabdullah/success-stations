3d96c3ed105b7fc345805577c540381a8ecd93ce
K P <soft.qureshi@gmail.com>
2021-08-23 21:30:26 +0500

    issues (#70)
    Co-authored-by: maryam cheema <maryam@codility.co>

--- lib/controller/user_profile_controller.dart
+++ lib/controller/user_profile_controller.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:success_stations/action/user_profile_action.dart';
 
 class UserProfileController extends GetxController {
 
   bool isLoading = false; 
   var userData;
+  GetStorage box = GetStorage();
     @override
   void onInit(){
     isLoading = true;
@@ -18,6 +20,7 @@ class UserProfileController extends GetxController {
     await userProfileAction().then((res) {
       print(">>>>>>>>>>>>>!!!!!!!!!!!!!!!!$res");
       userData = jsonDecode(res.body);
+      box.write('user_image',userData['data']['image']);
     
       isLoading = false;
       print("////////////////////////////////    $userData");
