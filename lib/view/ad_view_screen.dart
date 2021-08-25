a153b5b8581f771abbb7d442217634ccd46d6807
K P <soft.qureshi@gmail.com>
2021-08-25 12:48:00 +0500

    Fixes/ad posting bug (#81)
    * changes
    
    * changes
    
    * fixed
    
    * fixes
    
    * fixes
    
    Co-authored-by: LAP-073 <lap-073@vig38-h03-89-89-89-30.dsl.sta.abo.bbox.fr>

--- lib/view/ad_view_screen.dart
+++ lib/view/ad_view_screen.dart
@@ -315,13 +315,11 @@ Widget listTileRow2(data) {
                       AppTextStyles.appTextStyle(fontSize: 17, fontWeight: FontWeight.bold, color: Colors.grey,
                       ),
                     ),
-                      Container(
-                        width: Get.width/2.5,
-                        child: Text(data[index]['comment'][lang],style:
-                        AppTextStyles.appTextStyle(fontSize: 10, fontWeight: FontWeight.normal, color: Colors.grey,
-                        ),
-                    ),
+                      Text(data[index]['comment']['en'],style:
+                      AppTextStyles.appTextStyle(fontSize: 10, fontWeight: FontWeight.normal, color: Colors.grey,
                       ),
+                    ),
+                      
                   ],
                 ),
               )
