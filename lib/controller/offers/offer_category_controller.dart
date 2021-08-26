08734015a5884cd3e98200f789c95ead64424907
Asmat Ullah <asmat.ullah@codilitysolutions.com>
2021-08-26 18:04:53 +0500

    filter applied on  offer and api implementation

--- lib/controller/offers/offer_category_controller.dart
+++ lib/controller/offers/offer_category_controller.dart
@@ -3,26 +3,37 @@ import 'package:get/get.dart';
 import 'package:success_stations/action/offers/offer_category_action.dart';
 
 class OfferCategoryController extends GetxController {
-  bool isLoading = false; 
+  bool isLoading = false;
   List offeredList = [];
   var offerDattaTypeCategory;
+  var cData;
 
   @override
-  void onInit(){
+  void onInit() {
     isLoading = true;
     categoryOfferList();
     super.onInit();
   }
 
-  categoryOfferList() async{
+  categoryOfferList() async {
     print("controller call of the Favorite list");
     isLoading = true;
     await offersCategory().then((value) {
       print(",,,,,,,,,,,,,,,,,,,Offer data lisr.................$value");
       offerDattaTypeCategory = jsonDecode(value.body);
-      for(int c =0; c < offerDattaTypeCategory['data'].length; c++){
+      for (int c = 0; c < offerDattaTypeCategory['data'].length; c++) {
         offeredList.add(offerDattaTypeCategory['data'][c]);
       }
+      isLoading = false;
+    });
+    update();
+  }
+
+  offeraddedByIdAddes(id, userId) async {
+    isLoading = true;
+    await offersCategoryById(id, userId).then((res) {
+      cData = jsonDecode(res.body);
+     print("/////////////////// json offer by id .........................>>>>$cData");
       isLoading = false;
     });
     update();
