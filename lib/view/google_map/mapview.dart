5c5459a830de7757b7fd6c624dbd0f492a702e59
K P <soft.qureshi@gmail.com>
2021-08-27 19:58:29 +0500

    Feature/adds tab (#98)
    * user adds api implemented
    
    * user adds api implemented
    
    * user adds api implementation with call and fav icon
    
    * user adds api implementation with call and fav icon
    
    * user adds api implemented and its activities
    
    * user adds api implemented and its activities
    
    * user adds api implemented and its activities
    
    Co-authored-by: Asmat Ullah <asmat.ullah@codilitysolutions.com>

--- lib/view/google_map/mapview.dart
+++ lib/view/google_map/mapview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_place_picker/google_maps_place_picker.dart';
 import 'package:success_stations/controller/all_users_controller.dart';
@@ -9,9 +10,11 @@ import 'package:success_stations/controller/rating_controller.dart';
 import 'package:success_stations/styling/colors.dart';
 import 'package:success_stations/styling/images.dart';
 import 'package:success_stations/view/ad_views/ad_viewmain.dart';
+
 final ratingcont = Get.put(RatingController());
+
 class MapView extends StatefulWidget {
-  const MapView({ Key? key }) : super(key: key);
+  const MapView({Key? key}) : super(key: key);
 
   @override
   _MapViewState createState() => _MapViewState();
@@ -19,18 +22,19 @@ class MapView extends StatefulWidget {
 
 class _MapViewState extends State<MapView> {
   static final kInitialPosition = LatLng(-33.8567844, 151.213108);
-  
-     final banner = Get.put(BannerController());
+
+  final banner = Get.put(BannerController());
   @override
   void initState() {
     banner.bannerController();
     super.initState();
   }
-  var listtype = 'map'; 
+
+  var listtype = 'map';
   var grid = AppImages.gridOf;
-  Color listIconColor = Colors.grey; 
-  
-   Widget topWidget() {
+  Color listIconColor = Colors.grey;
+
+  Widget topWidget() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -38,78 +42,84 @@ class _MapViewState extends State<MapView> {
         child: Row(
           children: [
             IconButton(
-              onPressed: (){
-                setState(() {
-                  listtype = 'grid';
-                  listIconColor = Colors.grey;
-                  grid = AppImages.grid;
-                });             
-              },
-              icon: Image.asset(grid)
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 10,),
-              child: IconButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
-                    listtype = 'map';
-                    listIconColor = AppColors.appBarBackGroundColor;
-                    grid = AppImages.gridOf;
+                    listtype = 'grid';
+                    listIconColor = Colors.grey;
+                    grid = AppImages.grid;
                   });
                 },
-                icon: Icon(Icons.map,color: listIconColor,size: 45,)
+                icon: Image.asset(grid)),
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 10,
               ),
+              child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      listtype = 'map';
+                      listIconColor = AppColors.appBarBackGroundColor;
+                      grid = AppImages.gridOf;
+                    });
+                  },
+                  icon: Icon(
+                    Icons.map,
+                    color: listIconColor,
+                    size: 45,
+                  )),
             ),
-            SizedBox(height: 30,width: 15,)
+            SizedBox(
+              height: 30,
+              width: 15,
+            )
           ],
         ),
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     print(Get.height);
     return Scaffold(
       body: Stack(
         children: [
-           listtype == 'map' ? googleMap(kInitialPosition) :
-           GetBuilder<AllUsersController>( // specify type as Controller
-          init: AllUsersController(), // intialize with the Controller
-          builder: (value) { 
-           
-             return 
-             value.userData != null ?
-             allUsers(value.userData['data']): Center(child: CircularProgressIndicator());
-             } // value is an instance of Controller.
-          ),
+          listtype == 'map'
+              ? googleMap(kInitialPosition)
+              : GetBuilder<AllUsersController>(
+                  // specify type as Controller
+                  init: AllUsersController(), // intialize with the Controller
+                  builder: (value) {
+                    return value.userData != null
+                        ? allUsers(value.userData['data'])
+                        : Center(child: CircularProgressIndicator());
+                  } // value is an instance of Controller.
+                  ),
           Container(
             child: Row(
-              children: [
-                topWidget()
-              ],
+              children: [topWidget()],
             ),
           )
         ],
       ),
     );
   }
-} 
+}
 
-Widget googleMap(kInitialPosition){
-  
+Widget googleMap(kInitialPosition) {
   return Container(
     height: Get.height,
     child: PlacePicker(
       apiKey: "AIzaSyDS0wbOsjYPi6CaKvbs13USS5CUOc2D91c",
       initialPosition: kInitialPosition,
-          useCurrentLocation: true,
-          usePlaceDetailSearch: true,
-          onPlacePicked: (result) {
-            print(result.geometry!.location);
-            // selectedPlace = result;
-            // Navigator.of(context).pop();
-            // setState(() {});
-          },
+      useCurrentLocation: true,
+      usePlaceDetailSearch: true,
+      onPlacePicked: (result) {
+        print(result.geometry!.location);
+        // selectedPlace = result;
+        // Navigator.of(context).pop();
+        // setState(() {});
+      },
       // initialCameraPosition: CameraPosition(
       //   zoom: 15,
       //   target: LatLng(51.507351,-0.127758),
@@ -118,29 +128,39 @@ Widget googleMap(kInitialPosition){
   );
 }
 
+GetStorage box = GetStorage();
 
-
-Widget allUsers(userData){
-
+Widget allUsers(userData) {
   return GridView.builder(
-        padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 90, bottom: 10),
-        primary: false,
-        // padding: const EdgeInsets.all(0),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisSpacing: 10, mainAxisSpacing: 20, crossAxisCount: 2,
+      padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 90, bottom: 10),
+      primary: false,
+      // padding: const EdgeInsets.all(0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 20,
+        crossAxisCount: 2,
         childAspectRatio: Get.width /
-        (Get.height >= 800 ? Get.height/ 1.50 :Get.height <= 800 ? Get.height/ 1.80 :0),
-        ),
-        itemCount: userData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-             
-              Get.to(AdViewTab(),arguments: userData[index]['id']);
-            },
-            child: Card(
-              elevation: 3,
-              shape: RoundedRectangleBorder(
+            (Get.height >= 800
+                ? Get.height / 1.50
+                : Get.height <= 800
+                    ? Get.height / 1.80
+                    : 0),
+      ),
+      itemCount: userData.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            var j = userData[index]['id'];
+            var k = box.write('ind', j);
+            print("....hello hi...${box.read('ind')}");
+            print("....!!!!!!...!!!!!...1.......$j");
+
+            Get.to(AdViewTab(), arguments: userData[index]['id']);
+            //var cc=userData[index]['id']);
+          },
+          child: Card(
+            elevation: 3,
+            shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
             child: Container(
@@ -148,72 +168,77 @@ Widget allUsers(userData){
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   ClipRRect(
-                   borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0),topRight: Radius.circular(20.0)),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0)),
                     child: Container(
-                      width: Get.width/2.2,
-                        height: Get.height/5.2,
-                      child:userData[index]['image'] != null ? Image.network(userData[index]['image']['url']): Icon(Icons.image)
+                        width: Get.width / 2.2,
+                        height: Get.height / 5.2,
+                        child: userData[index]['image'] != null
+                            ? Image.network(userData[index]['image']['url'])
+                            : Icon(Icons.image)),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text(
+                      userData[index]['name'].toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
                     ),
                   ),
-                   Container(
-                     margin: EdgeInsets.only(left:20),
-                     child: Text(userData[index]['name'].toString(),
-                        style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 14),
-                        ),
-                   ),
-               Row(
-                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                 children: [
-                   Row(
-                    //
-                     children: [
-                       Container(
-                        //  margin: EdgeInsets.only(left:20),
-                         child: RatingBar.builder(
-                           ignoreGestures: true,
-                           initialRating: userData[index]['rating'].toDouble(),
-                           minRating: 1,
-                           direction: Axis.horizontal,
-                           allowHalfRating: true,
-                           itemCount: 5,
-                           itemSize: 13.5,
-                           // itemPadding: EdgeInsets.symmetric(horizontal: 3.0),
-                           
-                           itemBuilder: (context, _) => Icon(
-                             Icons.star,
-                             color: Colors.amber,
-                           ),
-                           
-                           onRatingUpdate:  (rating) { 
-                            //  var ratingjson = {
-                            //    'ads_id' : userData[index]['id'],
-                            //    'rate': rating
-                            //  };
-                            //  ratingcont.ratings(ratingjson );
-                            //  ratingcont.getratings(userData[index]['id']);
-                                       
-                           },
-                         ),
-                       ),
-                       Container(
-                         margin: EdgeInsets.only(left:5),
-                         child: Text("(${userData[index]['rating_count'].toString()})",style: TextStyle(fontSize: 13),)),
-                      
-                     ],
-                   ),
-                    Image.asset(AppImages.heart)
-                 ],
-               ),
-                     
-               
-                  
-         ],
-       ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        //
+                        children: [
+                          Container(
+                            //  margin: EdgeInsets.only(left:20),
+                            child: RatingBar.builder(
+                              ignoreGestures: true,
+                              initialRating:
+                                  userData[index]['rating'].toDouble(),
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemSize: 13.5,
+                              // itemPadding: EdgeInsets.symmetric(horizontal: 3.0),
+
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+
+                              onRatingUpdate: (rating) {
+                                //  var ratingjson = {
+                                //    'ads_id' : userData[index]['id'],
+                                //    'rate': rating
+                                //  };
+                                //  ratingcont.ratings(ratingjson );
+                                //  ratingcont.getratings(userData[index]['id']);
+                              },
+                            ),
+                          ),
+                          Container(
+                              margin: EdgeInsets.only(left: 5),
+                              child: Text(
+                                "(${userData[index]['rating_count'].toString()})",
+                                style: TextStyle(fontSize: 13),
+                              )),
+                        ],
+                      ),
+                      Image.asset(AppImages.heart)
+                    ],
+                  ),
+                ],
+              ),
             ),
-     ),
-          );
-          }
-  );
+          ),
+        );
+      });
 }
 //   return Container(
 //     margin: EdgeInsets.only(top:90,left: 15,right: 15),
